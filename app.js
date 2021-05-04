309d32c81243285e2b33d1feb67cf74636e88437
Antoine-Flo <antoine.flouzat@gmail.com>
2021-05-04 10:24:49 +0200

    easy pipe fn

--- app.js
+++ app.js
@@ -14,21 +14,34 @@ console.log(acc)
 
 const input = [1, 2, 3, 4, 5, 6]
 
+//Easy
 const output = input.reduce((acc, val, i) => {
     console.log(`|${i}| ${acc} + ${val} = ${acc + val}`)
     return acc + val
 })
 
+// One Line
+const output = input.reduce((acc, val) => acc + val)
+
 console.log(output)
+
 
 ///// Transformer en objet /////
 
-const input = ["🦊", "🦝", "🦍"]
-const color = ["Orange", "Gris", "Noir"]
-const output = input.reduce((acc, val, i) => ({...acc, [color[i]] : val}), {})
 
-const input = ["a", "b", 45, 4, 45, "a"]
-const output = input.reduce((a, v) => ({...a, [v]: ++[v] || 1}), {})
+const data = ['🦒', '🦝', '🐸', '🦝', '🐸', '🦝']
+
+//Easy
+const output = data.reduce((a, v, i) => {
+
+    a[v] = ++a[v] || 1
+    console.log(`|${i}| Value : ${v}`)
+    console.log(a)
+    return a
+}, {})
+
+// One Line
+const output = data.reduce((a, v) => ({...a, [v]: ++a[v] || 1}), {})
 
 console.log(output)
 
@@ -42,19 +55,37 @@ const output = mapLike(input, x => x + 1)
 
 console.log(output)
 
+
 ///////// Pipe function  /////////
 // Eric Elliott
 
 
-const input = 25
+const input = 7
 const add = x => x + 1
 const divide = x => x / 2
+const square = x => x*x
+// console.log(`V: ${v} | F: ${f}`)
 
-const pipe = (...fns) => x => fns.reduce((v, f) => {
-    console.log(`V: ${v} | F: ${f}`)
-    return f(v)
-}, x)
 
-const output = pipe(add, divide)(input)
+// Easy
+const pipe = function (...fns) {
+
+    console.log(fns)
+
+    return function (x) {
+
+        return fns.reduce((v, f) => {
+
+            console.log(`V: ${v} | F: ${f}`)
+            return f(v)
+        }, x)
+
+    }
+}
+
+// One line
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
+
+const output = pipe(add, divide, square)(input)
 
 console.log(output)
